b99295a5bc2920b6229b00ab0c6126aa5552b901
vatteh <victor@artivest.co>
2017-03-30 00:52:38 -0400

    added test dependencies

---
+++ src/components/Stream.spec.js
@@ -0,0 +1,14 @@
+import Stream from './Stream';
+import { shallow } from 'enzyme';
+
+describe('Stream', () => {
+    const props = {
+        tracks: [{ title: 'x' }, { title: 'y'}]
+    };
+
+    it('shows two elements', () => {
+        const element = shallow(<Stream { ...props } />);
+
+        expect(element.find('.track')).to.have.length(2);
+    }) 
+})
