7db00817c3a55059f4ec70fc705e9b0df814af8d
vatteh <victor@artivest.co>
2017-05-04 00:57:23 -0400

    get player to appear on play and look correctly

--- src/components/Player/presenter.js
+++ src/components/Player/presenter.js
@@ -2,14 +2,73 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
 import FontIcon from 'material-ui/FontIcon';
+import { GridList } from 'material-ui/GridList';
+import Slider from 'material-ui/Slider';
+
+const styles = {
+  playerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  nowPlayingContainer: {
+    flexGrow: 1,
+  },
+  playControlsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexGrow: 2,
+    textAlign: 'center',
+  },
+  volumeControlContainer: {
+    flexGrow: 1,
+  },
+  controlButtons: {
+    width: 150,
+    display: 'flex',
+    flexGrow: 2,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  slider: {
+    flexGrow: 1,
+    width: 800,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  playPauseIcon: {
+    flexGrow: 1,
+  },
+  nextPrevSong: {
+    flexGrow: 1,
+  },
+};
 
 export default class Player extends Component {
   render() {
-    const { activeTrack } = this.props;
+    const { activeTrack, isPlaying, onPlayPauseIconClick } = this.props;
 
     return (
-      <div className={`player ${activeTrack ? 'player-visible' : ''}`}>
-        <span>Player</span>
+      <div className={`player ${activeTrack ? 'player-visible' : ''}`} style={styles.playerContainer}>
+        <div style={styles.nowPlayingContainer}>now playing</div>
+        <div style={styles.playControlsContainer}>
+          <div style={styles.controlButtons}>
+            <i className="fa fa-step-backward fa-2x" aria-hidden="true" style={styles.nextPrevSong}></i>
+            <span onClick={onPlayPauseIconClick.bind(this, activeTrack)} style={styles.playPauseIcon}>
+              {
+                isPlaying ?
+                <i className="fa fa-pause-circle-o fa-4x" aria-hidden="true"></i> :
+                <i className="fa fa-play-circle-o fa-4x" aria-hidden="true"></i>
+              }
+            </span>
+            <i className="fa fa-step-forward fa-2x" aria-hidden="true" style={styles.nextPrevSong}></i>
+          </div>
+          <Slider min={0} max={1000} step={1} sliderStyle={styles.slider}/>
+        </div>
+        <div style={styles.volumeControlContainer}>volume control</div>
       </div>
     );
   }
