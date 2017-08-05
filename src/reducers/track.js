88e8065cf37a35b0ddf8309ad4817b36dcf4fdf0
vatteh <victor@artivest.co>
2017-08-05 17:31:12 -0400

    fix slider bug on new track

--- src/reducers/track.js
+++ src/reducers/track.js
@@ -7,44 +7,48 @@ const initialState = {
   currentTrackTime: 0,
 };
 
-export default function (state = initialState, action) {
-  const stateCopy = { ...state };
-  stateCopy.seekTo = false;
+export default function (prevState = initialState, action) {
+  const newState = { ...prevState };
+  newState.seekTo = false;
 
   switch (action.type) {
     case actionTypes.TRACKS_SET: {
       const { tracks } = action;
-      stateCopy.tracks = tracks;
-      return stateCopy;
+      newState.tracks = tracks;
+      return newState;
     }
     case actionTypes.TRACK_PLAY: {
       const { track } = action;
-      stateCopy.activeTrack = track;
-      stateCopy.isPlaying = true;
-      return stateCopy;
+      newState.activeTrack = track;
+      newState.isPlaying = true;
+      if (newState.activeTrack !== prevState.activeTrack) {
+        newState.currentTrackTime = 0.01; // weird slider bug when currentTrackTime is set to 0
+      }
+      return newState;
     }
     case actionTypes.TRACK_PAUSE: {
-      stateCopy.isPlaying = false;
-      return stateCopy;
+      newState.isPlaying = false;
+      return newState;
     }
     case actionTypes.TRACK_NEXT_PREV: {
       const { track } = action;
-      stateCopy.isPlaying = true;
-      stateCopy.activeTrack = track;
-      return stateCopy;
+      newState.activeTrack = track;
+      newState.isPlaying = true;
+      newState.currentTrackTime = 0.01; // weird slider bug when currentTrackTime is set to 0
+      return newState;
     }
     case actionTypes.UPDATE_TRACK_PLAYHEAD: {
       const { currentTrackTime } = action;
-      stateCopy.seekTo = true;
-      stateCopy.currentTrackTime = currentTrackTime;
-      return stateCopy;
+      newState.seekTo = true;
+      newState.currentTrackTime = currentTrackTime;
+      return newState;
     }
     case actionTypes.UPDATE_SLIDER: {
       const { currentTrackTime } = action;
-      stateCopy.currentTrackTime = currentTrackTime;
-      return stateCopy;
+      newState.currentTrackTime = currentTrackTime;
+      return newState;
     }
     default:
-      return stateCopy;
+      return newState;
   }
 }
