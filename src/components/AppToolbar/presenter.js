62ab87e7707554929a92c1a9cebb0c38024b370d
vatteh <victor@artivest.co>
2017-08-18 20:36:35 -0400

    add searched for text in toolbar

--- src/components/AppToolbar/presenter.js
+++ src/components/AppToolbar/presenter.js
@@ -7,6 +7,15 @@ const styles = {
   searchIcon: {
     marginRight: 10,
   },
+  underlineStyle: {
+    borderColor: 'black',
+  },
+  underlineFocusStyle: {
+    borderColor: 'rgb(224, 224, 224)',
+  },
+  searchedFor: {
+    marginRight: 10,
+  },
 };
 
 export default class AppToolbar extends Component {
@@ -14,21 +23,29 @@ export default class AppToolbar extends Component {
     const { onFetchTracks, updateSearchText } = this.props;
 
     if (e.key === 'Enter') {
-      onFetchTracks(e.target.value);
       updateSearchText(e.target.value);
+      onFetchTracks(e.target.value);
       e.target.value = '';
     }
   }
 
   render() {
+    const { searchText } = this.props;
+
     return (
       <AppBar
         title="Soundcloud-DJ"
         showMenuIconButton={false}
         iconElementRight={
           <span>
+            {searchText && <span style={styles.searchedFor}>{`Searched for: ${searchText}`}</span>}
             <i className="fa fa-search fa-lg" style={styles.searchIcon} aria-hidden="true" />
-            <TextField id="search-text-field" onKeyPress={this.onKeyPress.bind(this)} />
+            <TextField
+              id="search-text-field"
+              onKeyPress={this.onKeyPress.bind(this)}
+              underlineStyle={styles.underlineStyle}
+              underlineFocusStyle={styles.underlineFocusStyle}
+            />
           </span>
         }
       />
